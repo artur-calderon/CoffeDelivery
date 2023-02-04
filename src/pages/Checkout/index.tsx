12e26a2a23896e224e75498d581c19514edecdc8
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2023-02-03 20:21:18 -0400

    page checkout styles

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,29 @@
-import { CheckoutContainer } from "./styles";
+
+import { MapPinLine } from "phosphor-react";
+import { Cart, CheckoutContainer, Form, FormCheckOut, Title } from "./styles";
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <h1>Checkout</h1>:
-    </CheckoutContainer>
+      <FormCheckOut>
+        <h1>Complete seu pedido</h1>
+        <Form>
+          <Title>
+            <MapPinLine size={32} weight="thin" />
+            <div>
+              <p>Endereço de Entrega</p>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </Title>
+
+          <input type="text" name="CEP" placeholder="CEP" />
+          <input type="text" name="RUA" placeholder="Rua" />
+
+        </Form>
+      </FormCheckOut>
+      <Cart>
+
+      </Cart>
+    </CheckoutContainer >
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,5 +1,60 @@
 import styled from "styled-components";
 
 export const CheckoutContainer = styled.div`
+  display: flex;
 
 `
+export const FormCheckOut = styled.div`
+  display: flex;
+  flex-direction: column;
+
+
+  h1{
+    font-family: ${props => props.theme.Baloo2};
+    font-size: 1.125rem;
+    margin-bottom: .9375rem;
+    margin-top: 2.5rem;
+  }
+
+  `
+
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  background-color: ${props => props.theme["base-card"]};
+
+  width: 40rem;
+  height: 23.25rem;
+
+ 
+
+
+`
+export const Title = styled.div`
+  display: flex;
+ 
+
+  gap: .5rem;
+  font-family: ${props => props.theme.Roboto};
+
+  div p:nth-child(1){
+    font-size: 1rem;
+    font-weight: 400;
+    color: ${props => props.theme["base-subtitle"]};
+  }
+  div p:nth-child(2){
+    font-size: .875rem;
+    font-weight: 400;
+    color: ${props => props.theme["base-text"]};
+  }
+
+  svg{
+    color:${props => props.theme["yellow-dark"]};
+    width: 1.5rem;
+    margin-top: -.375rem;
+  }
+
+`
+export const Cart = styled.div``
