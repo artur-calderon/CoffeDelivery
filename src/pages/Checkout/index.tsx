e318822f764cecc45cc67a06b6ce252c04518900
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2023-02-05 19:05:18 -0400

    Checkout page layout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,6 +1,6 @@
 
-import { MapPinLine } from "phosphor-react";
-import { Cart, CheckoutContainer, Form, FormCheckOut, Title } from "./styles";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+import { Cart, CartContainer, CheckoutContainer, Form, FormCheckOut, Pagamento, Title } from "./styles";
 
 export function Checkout() {
   return (
@@ -15,14 +15,48 @@ export function Checkout() {
               <p>Informe o endereço onde deseja receber seu pedido</p>
             </div>
           </Title>
-
-          <input type="text" name="CEP" placeholder="CEP" />
-          <input type="text" name="RUA" placeholder="Rua" />
-
+          <div className="first">
+            <input type="text" name="CEP" placeholder="CEP" />
+            <input type="text" name="RUA" placeholder="Rua" />
+          </div>
+          <div className="secondGroupInput">
+            <input type="text" name="numero" placeholder="Número" />
+            <input type="text" name="complemento" placeholder="Complemento" />
+            <input type="text" name="bairro" placeholder="Bairro" />
+            <input type="text" name="Cidade" placeholder="Cidade" />
+            <input type="text" name="UF" placeholder="UF" />
+          </div>
         </Form>
+
+        <Pagamento>
+          <Title>
+            <CurrencyDollar size={32} weight="thin" color="#8047F8" />
+            <div>
+              <p>Pagamento</p>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </div>
+          </Title>
+          <div className="Buttons">
+            <button>
+              <CreditCard size={20} weight="thin" color="#8047F8" />
+              <span>CARTÃO DE CRÉDITO</span>
+            </button>
+            <button>
+              <Bank size={20} weight="thin" color="#8047F8" />
+              <span>CARTÃO DE DÉBITO</span>
+            </button>
+            <button>
+              <Money size={20} weight="thin" color="#8047F8" />
+              <span>DINHEIRO</span>
+            </button>
+          </div>
+        </Pagamento>
       </FormCheckOut>
       <Cart>
+        <h1>Cafés selecionados</h1>
+        <CartContainer>
 
+        </CartContainer>
       </Cart>
     </CheckoutContainer >
   )
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const CheckoutContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap:2rem;
 
 `
 export const FormCheckOut = styled.div`
@@ -26,15 +29,66 @@ export const Form = styled.form`
   background-color: ${props => props.theme["base-card"]};
 
   width: 40rem;
-  height: 23.25rem;
+  border-radius:6px;
 
+  input{
+    
+    border: 1px solid ${props => props.theme["base-button"]};
+    border-radius: 4px;
+    background-color: ${props => props.theme["base-input"]};
+    padding: .75rem;
+    outline: none;
+    height: 2.625rem;
+
+    font-family: ${props => props.theme.Roboto};
+    color: ${props => props.theme["base-label"]};
+    font-size: .875rem;
+  }
+  .first{
+    display: flex;
+    flex-direction: column;
+    gap:1rem;
+    margin-bottom: 1rem;
+    input:nth-child(1){
+      width: 12.5rem;
+   }
+  }
  
+   .secondGroupInput{
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      
+
+    input:nth-child(1){
+      width: 12.5rem;
+      margin-bottom: .75rem;
+    }
+    input:nth-child(2){
+      flex: 1;
+      margin-bottom: 1rem;
+      margin-left: .75rem;
+    }
+  
+    input:nth-child(3){
+      width: 12.5rem;
+      margin-right: 0.75rem;
+    }
+    input:nth-child(4){
+      width: 15.75rem;
+    }
+    input:nth-child(5){
+      width: 3.75rem;
+      margin-left: .75rem;
+    }
+
+   }
 
 
 `
 export const Title = styled.div`
   display: flex;
- 
+  margin-bottom: 2rem;
 
   gap: .5rem;
   font-family: ${props => props.theme.Roboto};
@@ -57,4 +111,63 @@ export const Title = styled.div`
   }
 
 `
-export const Cart = styled.div``
+
+export const Pagamento = styled.div`
+  margin-top: .75rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  padding: 2.5rem;
+  background-color: ${props => props.theme["base-card"]};
+
+  
+  .Buttons{
+    display: flex;
+    gap: .75rem;
+
+
+    button{
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      width: 11.1669rem;
+      gap: .3rem;
+      border:0;
+      width: 11.1669rem;
+      height: 3.1875rem;
+      border-radius: 6px;
+      background-color: ${props => props.theme["base-button"]};
+      padding: 1rem;
+
+      cursor: pointer;
+      font-size: 0.8rem;
+      font-family: ${props => props.theme.Roboto};
+      color: ${props => props.theme["base-text"]};
+    }
+
+   
+  }
+  
+`
+
+
+
+export const Cart = styled.div`
+
+  h1{
+    font-family: ${props => props.theme.Baloo2};
+    font-size: 1.125rem;
+    margin-bottom: .9375rem;
+    margin-top: 2.5rem;
+  }
+`
+
+export const CartContainer = styled.div`
+  width: 28rem;
+  height: 31.125rem;
+  background-color: ${props => props.theme["base-card"]};
+  padding: 3rem;
+
+  border-radius: 6px 44px;
+
+`
